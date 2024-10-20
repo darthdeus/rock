f9976ef557ffb594a1c4cde6214d46ff5d37169f
Jakub Arnold <darthdeus@gmail.com>
2024-10-20 18:18:36 +0200

    Add example lsp server

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+lsp:
+	cd rock-lsp && RUST_LOG=lsp_server=debug cargo run
+
 default:
 	make -C tree-sitter-rock all
 	cd rock-fmt && cargo run -- ../tree-sitter-rock/examples/function.rock
--- rock-lsp/src/main.rs
+++ rock-lsp/src/main.rs
@@ -1,3 +1,91 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use lsp_server::{Connection, ExtractError, Message, Request, RequestId, Response};
+use lsp_types::{
+    request::GotoDefinition, GotoDefinitionResponse, InitializeParams, OneOf, ServerCapabilities,
+};
+
+fn main() -> Result<()> {
+    eprintln!("Starting Rock LSP server ...");
+
+    let (connection, io_threads) = Connection::stdio();
+
+    let server_capabilities = serde_json::to_value(&ServerCapabilities {
+        definition_provider: Some(OneOf::Left(true)),
+        ..Default::default()
+    })?;
+
+    let initialization_params = match connection.initialize(server_capabilities) {
+        Ok(it) => it,
+        Err(e) => {
+            if e.channel_is_disconnected() {
+                io_threads.join()?;
+            }
+
+            return Err(e.into());
+        }
+    };
+
+    main_loop(connection, initialization_params)?;
+
+    io_threads.join()?;
+    eprintln!("Shutting down Rock LSP  server");
+
+    Ok(())
+}
+
+fn main_loop(connection: Connection, params: serde_json::Value) -> Result<()> {
+    let _params: InitializeParams = serde_json::from_value(params)?;
+    eprintln!("Starting example main loop");
+
+    for msg in &connection.receiver {
+        eprintln!("got msg: {msg:?}");
+
+        match msg {
+            Message::Request(req) => {
+                if connection.handle_shutdown(&req)? {
+                    return Ok(());
+                }
+
+                eprintln!("got request: {req:?}");
+
+                match cast::<GotoDefinition>(req) {
+                    Ok((id, params)) => {
+                        eprintln!("got gotoDefinition request #{id}: {params:?}");
+
+                        let result = Some(GotoDefinitionResponse::Array(Vec::new()));
+                        let result = serde_json::to_value(&result)?;
+                        let resp = Response {
+                            id,
+                            result: Some(result),
+                            error: None,
+                        };
+
+                        connection.sender.send(Message::Response(resp))?;
+                        continue;
+                    }
+
+                    Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    Err(ExtractError::MethodMismatch(req)) => req,
+                };
+            }
+
+            Message::Response(resp) => {
+                eprintln!("got response: {resp:?}");
+            }
+
+            Message::Notification(not) => {
+                eprintln!("got notification: {not:?}");
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn cast<R>(req: Request) -> Result<(RequestId, R::Params), ExtractError<Request>>
+where
+    R: lsp_types::request::Request,
+    R::Params: serde::de::DeserializeOwned,
+{
+    req.extract(R::METHOD)
 }
